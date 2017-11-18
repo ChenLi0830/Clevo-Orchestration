37d2e7ce5f44b00b98825e1ad6b81de02d4a35d8
Chen Li <yichen.li0830@gmail.com>
2017-11-17 22:21:59 -0800

    Adding tests

---
+++ app/__test__/testFormatAudio.js
@@ -0,0 +1,20 @@
+const formatAudio = require('../services/formatAudio')
+const debug = require('debug')('test')
+require('dotenv').config({path: '../.env'})
+
+const data = {
+  audioUrl: 'http://companies-calls.oss-cn-beijing.aliyuncs.com/20170625083904_960_18841323731_601.mp3',
+  encoding: 'pcm_s16le',
+  channel: 1,
+  sampleRate: 8000
+}
+
+module.exports = function () {
+  return formatAudio(data)
+    .then(result => {
+      // const {sentenceCategories, topicCategory} = result
+      // debug('sentenceCategories', sentenceCategories)
+      debug('result', result)
+      return result
+    })
+}

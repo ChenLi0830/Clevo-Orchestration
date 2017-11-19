5eb6b76e9f203ef38fe91d9b927138beb4571c6a
Chen Li <yichen.li0830@gmail.com>
2017-11-18 21:41:24 -0800

    call categorization-service in series rather than in parallel. Add async-retry to retry on errors

--- app/services/categorization.js
+++ app/services/categorization.js
@@ -1,6 +1,10 @@
 require('es6-promise').polyfill()
 const debug = require('debug')('categorization-service')
 const {createApolloFetch} = require('apollo-fetch')
+const retry = require('async-retry')
+
+const MAX_RETRY = 3
+let trialCount = 0
 
 const getSpeechCategorization = (aggregatedTranscription) => {
   return new Promise((resolve, reject) => {
@@ -9,8 +13,6 @@ const getSpeechCategorization = (aggregatedTranscription) => {
 }
 
 const getSentenseCategorization = (sentence) => {
-  debug('endpoint', process.env.SENTENCE_CATEGORIZATION_ENDPOINT || `http://localhost:3030/graphql`)
-
   const fetch = createApolloFetch({
     uri: process.env.SENTENCE_CATEGORIZATION_ENDPOINT || `http://localhost:3030/graphql`
   })
@@ -36,6 +38,15 @@ const getSentenseCategorization = (sentence) => {
       }
       return data.categorizeSentence
     })
+    .catch(error => {
+      console.log('error: ', error)
+      console.log('trialCount', trialCount, 'MAX_RETRY', MAX_RETRY)
+      if (trialCount < MAX_RETRY) {
+        console.log('Retry...')
+        trialCount++
+        return getSentenseCategorization(sentence)
+      }
+    })
 }
 
 function saveServiceResult ({_id, result}) {
@@ -43,7 +54,7 @@ function saveServiceResult ({_id, result}) {
     uri: process.env.SERVER_ENDPOINT || `http://localhost:4000/graphql`
   })
 
-  debug('result', result)
+  debug('Saving Categorization Result...')
   const query = `
     mutation updateCall(
       $id: MongoID!
@@ -118,10 +129,19 @@ function saveServiceResult ({_id, result}) {
   })
 
     .then(body => {
-      debug('body', body)
-      debug('body.data', body.data)
-      debug('body.data.callUpdate', body.data.callUpdate)
+      debug('Result is saved successfully', body)
+      // debug('body.data', body.data)
+      // debug('body.data.callUpdate', body.data.callUpdate)
       return body.data.callUpdate
+    })
+    .catch(error => {
+      console.log('error: ', error)
+      console.log('trialCount', trialCount, 'MAX_RETRY', MAX_RETRY)
+      if (trialCount < MAX_RETRY) {
+        console.log('Retry...')
+        trialCount++
+        return saveServiceResult({_id, result})
+      }
     })
 }
 
@@ -140,37 +160,74 @@ function callService (transcriptionResult) {
   let speechTranscription = sentenceList.reduce((speech, sentence) => speech + sentence, '')
   nlpPromises.push(getSpeechCategorization(speechTranscription))
 
-  // 每句对话
-  sentenceList.forEach(transcription => {
-    let singleSentensePromise = getSentenseCategorization(transcription.onebest)
-      .then(category => {
-        return {
+  // 每句对话 - 串行处理
+  async function categorizeInSeries (sentenceList) {
+    let results = []
+    // let index = 0
+    for (const transcription of sentenceList) {
+      // index += 1
+      // let retries = 0
+      await retry(async bail => {
+        let category = await getSentenseCategorization(transcription.onebest)
+        let categoryResult = {
           bg: transcription.bg,
           ed: transcription.ed,
           speaker: transcription.speaker,
           categories: category.categoriesOfSentence
           // categories: category,
         }
+        debug('categoryResult', categoryResult)
+        // if (index === 2) {
+        //   if (retries <= 2) {
+        //     retries++
+        //     throw new Error('test error!')
+        //   }
+        // }
+        results.push(categoryResult)
+      }, {
+        retries: 3,
+        factor: 1
       })
+    }
+    debug('categorizationFinalResult', results)
+    return results
+  }
+  return categorizeInSeries(sentenceList)
 
-    nlpPromises.push(singleSentensePromise)
-  })
+  // // 每句对话 - 并行处理
+  // sentenceList.forEach(transcription => {
+  //   let singleSentensePromise = getSentenseCategorization(transcription.onebest)
+  //     .then(category => {
+  //       return {
+  //         bg: transcription.bg,
+  //         ed: transcription.ed,
+  //         speaker: transcription.speaker,
+  //         categories: category.categoriesOfSentence
+  //         // categories: category,
+  //       }
+  //     })
 
-  return Promise.all(nlpPromises)
-    .then(results => {
-      // debug('results', JSON.stringify(results))
-      let categorizeResult = {}
-      categorizeResult.topicCategory = results[0]
-      categorizeResult.sentenceCategories = results.slice(1)
+  //   nlpPromises.push(singleSentensePromise)
+  // })
 
-      return categorizeResult
-    })
+  // return Promise.all(nlpPromises)
+  //   .then(results => {
+  //     // debug('results', JSON.stringify(results))
+  //     debug('Categorization finished.')
+  //     let categorizeResult = {}
+  //     categorizeResult.topicCategory = results[0]
+  //     categorizeResult.sentenceCategories = results.slice(1)
+
+  //     return categorizeResult
+  //   })
 }
 
 function CategorizeAndSave (transcriptionResult) {
+  debug('endpoint', process.env.SENTENCE_CATEGORIZATION_ENDPOINT || `http://localhost:3030/graphql`)
+
   return callService(transcriptionResult)
     .then(categorizationResult => {
-      debug('categorizationResult.topicCategory')
+      // debug('categorizationResult.topicCategory')
       // debug('Audio file has been transcribed: ', transcriptionResult)
       // debug('url', url)
       return saveServiceResult({_id: transcriptionResult.recordId, result: categorizationResult})
